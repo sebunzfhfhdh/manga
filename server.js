a672d4d8ff4b0e48f646791e1e4a281095294c2d
sebunzfhfhdh <131063220+sebunzfhfhdh@users.noreply.github.com>
2024-12-01 15:08:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Dynamic port for Render
 const imageRoutes = require('./routes/images');
-const { runEvery12Hours } = require('./updater'); // Import updater
 
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((err, req, res, next) => {
-    console.error('Global Error Handler:', err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-});
 
 app.use('/manga', require('./routes/manga'));
 app.use('/images', imageRoutes);
@@ -22,20 +16,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the updater in the background when the server starts
-const startUpdater = async () => {
-    try {
-        console.log('Starting the manga update process...');
-        await runEvery12Hours(); // Corrected this line
-        console.log('Manga updates completed successfully.');
-    } catch (err) {
-        console.error('Error running the manga update:', err.message);
-    }
-};
+app.get('/doc', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'doc.html'));
+});
+
 
 
 // Start the Express server
 app.listen(port, () => {
-    console.log(`Server is running`);
-    startUpdater();
+    console.log(`Server is running on port: ${port}`);
 });
