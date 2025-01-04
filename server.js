7fee6b75308400ff9471a8ffec9eaa2f82e6f8ac
sebunz <101024918+sebunz@users.noreply.github.com>
2025-01-03 16:45:47 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000; // Dynamic port for Render
+const port = process.env.PORT || 5000; 
 const favicon = require('serve-favicon');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Serve static files (e.g., images, CSS, JS) from the "public" directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/latest', require('./routes/latest'));
 app.use('/popular', require('./routes/popular'));
 app.use('/manga', require('./routes/manga'));
@@ -19,18 +18,17 @@ app.use('/search', require('./routes/search'));
 app.use('/images', require('./routes/images'));
 app.use('/add', require('./routes/likes'));
 
-// Serve the main page (index.html) when accessing the root route
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
-    console.error(err.stack);  // Log the error for debugging
+    console.error(err.stack); 
     res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
 
-// Start the Express server
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
